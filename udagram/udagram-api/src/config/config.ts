fa1100b262186891be24210384497ec47a468762
Aisha Soliman <aeshasoliman12@gmail.com>
2023-02-03 23:59:55 +0200

    fix

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -15,6 +15,7 @@ export const config = {
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
+  postgres_port: process.env.POSTGRES_PORT,
   jwt: {
     secret: process.env.JWT_SECRET,
   },
